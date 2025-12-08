3b23c51fe7de6f20b4abfe6d4427fd4a58a0652d
BrennaPe <brennape47@icloud.com>
2025-12-08 07:04:19 +0000

    EXERCICIOS JS

---
+++ JavaScript/moduloF-cursoEmVideo/variaveisCompostas.js
@@ -0,0 +1,7 @@
+let num = [5, 6, 7, 3, 1]
+num[5]= 1
+num.sort()
+num.push(9)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
